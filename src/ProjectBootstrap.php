f412948431e5bcd3376dad682ecc7d1d757bb062
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-05-15 11:39:17 +0100

    Added command provider class ready to add composer command(s)

--- src/ProjectBootstrap.php
+++ src/ProjectBootstrap.php
@@ -8,14 +8,17 @@ namespace CubicMushroom\Composer\Plugin;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Plugin\Capability\CommandProvider as ComposerCommandProvider;
+use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
+use CubicMushroom\Composer\Plugin\ProjectBootstrap\CommandProvider as ProjectBootstrapCommandProvider;
 
 /**
  * Main plugin class
  *
  * @package CubicMushroom\Composer\Plugin\ProjectBootstrap
  */
-class ProjectBootstrap implements PluginInterface
+class ProjectBootstrap implements PluginInterface, Capable
 {
     /**
      * Apply plugin modifications to Composer
@@ -26,5 +29,31 @@ class ProjectBootstrap implements PluginInterface
     public function activate(Composer $composer, IOInterface $io)
     {
         // Do nothing here (yet?)
+    }
+
+
+    /**
+     * Method by which a Plugin announces its API implementations, through an array
+     * with a special structure.
+     *
+     * The key must be a string, representing a fully qualified class/interface name
+     * which Composer Plugin API exposes.
+     * The value must be a string as well, representing the fully qualified class name
+     * of the implementing class.
+     *
+     * @tutorial
+     *
+     * return array(
+     *     'Composer\Plugin\Capability\CommandProvider' => 'My\CommandProvider',
+     *     'Composer\Plugin\Capability\Validator'       => 'My\Validator',
+     * );
+     *
+     * @return string[]
+     */
+    public function getCapabilities()
+    {
+        return [
+            ComposerCommandProvider::class => ProjectBootstrapCommandProvider::class,
+        ];
     }
 }
---
+++ src/ProjectBootstrap/CommandProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Author: Toby Griffiths <toby@cubicmushroom.co.uk>
+ * Created: 15/05/2016
+ */
+
+namespace CubicMushroom\Composer\Plugin\ProjectBootstrap;
+
+use Composer\Command\BaseCommand;
+use Composer\Plugin\Capability\CommandProvider as CommandProviderCapability;
+
+/**
+ * Registered additional composer commands
+ *
+ * @package CubicMushroom\Composer\Plugin\ProjectBootstrap
+ */
+class CommandProvider implements CommandProviderCapability
+{
+
+    /**
+     * Retreives an array of commands
+     *
+     * @return BaseCommand[]
+     */
+    public function getCommands()
+    {
+        return [];
+    }
+}
